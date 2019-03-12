d3326154a0dce4dbf424985c3467d282f5bb89b2
Benson Sanga <Bensonsanga@hotmail.com>
2019-03-12 10:24:45 +0000

    all the rest

--- list01.c
+++ list01.c
@@ -3,5 +3,17 @@
 
 int main(void)
 {
+    int *numbers = NULL;
+    int capacity = 0;
     
+    int size = 0;
+    while (true)
+    {
+        int number = get_int("Number: ");
+        
+        if (size==capacity)
+        {
+            numbers = realloc(numbers, sizeof(int)*)
+        }
+    }
 }
